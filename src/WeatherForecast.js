f028c0ffe4da9ea6a19dc53da571a7624fadd9f9
EstherAyankoya <estherayankoya@gmail.com>
2023-10-23 02:34:38 +0100

    api 2

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
 
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+
+    let apiKey = "597c40c39084687093b091cd48b366f8";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className="WeatherForecast">
             <div className="row">
@@ -16,7 +28,6 @@ export default function WeatherForecast() {
                         <span className="WeatherForecast-temperatures-min">5</span>
                     </div>
                 </div>
-                
             </div>
         </div>
     );
